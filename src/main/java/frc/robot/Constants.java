0a23b54ff811ffdd69c7bb4bca1553d655ec8fe3
einstein2823 <devs@hprobotics.org>
2026-02-21 11:22:03 -0600

    changed hood top and bottom constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -224,8 +224,8 @@ public final class Constants {
     }
 
     public static class HoodConstants {
-        public static final double hoodBottom = -0.41;
-        public static final double hoodTop = 2.15;
+        public static final double hoodBottom = 0;
+        public static final double hoodTop = 2.56;
         public static final double kP = 1.5;
         public static final double kD = 0.01;
         public static final double maxSpeed = 0.2;
