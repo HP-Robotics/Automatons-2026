d418ad0b1dd9e99336fac8fe667c956024c12b9e
einstein2823 <devs@hprobotics.org>
2026-03-03 21:48:19 -0600

    intake speed tweak

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -38,7 +38,7 @@ public final class Constants {
     }
 
     public static class IntakeConstants {
-        public static final double speed = -0.8;
+        public static final double speed = -0.5;
         public static final double kP = 1;
         public static final double kI = 0;
         public static final double kD = 0;
