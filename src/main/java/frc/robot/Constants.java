f4d96a910d163704f5511b89056c6ebc2ad871b4
einstein2823 <devs@hprobotics.org>
2026-03-23 20:24:36 -0500

    lower shooter kp

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -160,7 +160,7 @@ public final class Constants {
         public static final double defaultShootingSpeed = 54.2; // rotations per second
         public static final double idleSpeed = 10; // rotations per second
         // TODO: pick this
-        public static final double kP = 0.8;
+        public static final double kP = 0.7;
         public static final double kI = 0.2;
         public static final double kD = 0.0;
         public static final double kS = 0.25;
